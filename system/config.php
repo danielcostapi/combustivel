bf4008d76670af7dd2d4b1fc00433da77b430d97
danielcostapi <danielluis2@gmail.com>
2017-08-05 10:06:39 -0300

    Projeto

---
+++ system/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOSTNAME', 'localhost' );
+define('DB_USERNAME', 'root' );	
+define('DB_PASSWORD', '' );	
+define('DB_DATABASE', 'sysfrota' );	
+define('DB_CHARSET', 'utf8' );
+define('DB_PREFIX', 'bd' );	
+
+$direitos = 'Developer: <b>Daniel</b> Costa (DL Sistemas)';
+$titulo = 'SysFrota -  1.0';
+	
